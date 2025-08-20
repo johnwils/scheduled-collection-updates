5f3df54fb64980faf8a86404d8475b619c960e2c
John Wilson <johnner@me.com>
2025-08-20 14:58:17 -0700

    add zodern types

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "johnner:scheduled-collection-updates",
-  version: "1.0.1",
+  version: "1.0.2",
   summary:
     "Schedule conditional MongoDB collection updates (reactive, restart-safe, multi-container).",
   git: "https://github.com/johnwils/scheduled-collection-updates",
@@ -9,13 +9,25 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.versionsFrom("3.0.3");
-  api.use(["ecmascript", "typescript", "mongo", "meteor"]);
+  api.use([
+    "ecmascript",
+    "typescript",
+    "mongo",
+    "meteor",
+    "zodern:types@1.0.13",
+  ]);
   api.mainModule("index.ts", "server");
 });
 
 Package.onTest(function (api) {
   api.versionsFrom("3.0.3");
-  api.use(["ecmascript", "typescript", "mongo", "meteor"]);
+  api.use([
+    "ecmascript",
+    "typescript",
+    "mongo",
+    "meteor",
+    "zodern:types@1.0.13",
+  ]);
   api.use("tinytest");
   api.use("johnner:scheduled-collection-updates");
   api.mainModule("tests.ts", "server");
